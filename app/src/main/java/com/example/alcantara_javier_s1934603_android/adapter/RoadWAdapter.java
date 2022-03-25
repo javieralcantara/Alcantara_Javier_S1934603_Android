334327ead844c98442ec9f2a81cf732b2ce908ef
javieralcantara <GaRC1a.1995.1998>
2022-03-25 17:17:24 +0100

    colour scheme

--- app/src/main/java/com/example/alcantara_javier_s1934603_android/adapter/RoadWAdapter.java
+++ app/src/main/java/com/example/alcantara_javier_s1934603_android/adapter/RoadWAdapter.java
@@ -1,5 +1,7 @@
 package com.example.alcantara_javier_s1934603_android.adapter;
 import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,11 +9,16 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+
 import com.example.alcantara_javier_s1934603_android.R;
 import com.example.alcantara_javier_s1934603_android.model.Item;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 
 
 public class RoadWAdapter extends ArrayAdapter<Item> {
@@ -25,6 +32,7 @@ public class RoadWAdapter extends ArrayAdapter<Item> {
         itemsList = list;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View listItem = convertView;
@@ -41,6 +49,20 @@ public class RoadWAdapter extends ArrayAdapter<Item> {
 
         TextView date = (TextView) listItem.findViewById(R.id.date);
         date.setText(currentItem.getParsedDate());
+
+        Instant now = Instant.now();
+
+        Instant sixMonthAgo = now.minus(180, ChronoUnit.DAYS);
+        Instant oneMonthAgo = now.minus(30, ChronoUnit.DAYS);
+
+
+        if(currentItem.getDate().before(Date.from(sixMonthAgo))) {
+            date.setTextColor(Color.RED);
+        } else if (currentItem.getDate().before(Date.from(oneMonthAgo))) {
+            date.setTextColor(Color.rgb(255,140,0));
+        } else {
+            date.setTextColor(Color.rgb(0,100,0));
+        }
 
         return listItem;
     }
